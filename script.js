96e5870f9bf0055f9740ddab2662fa6fd662d514
Eishalex2 <t.whitney.a@gmail.com>
2022-11-24 15:52:48 -0800

    feat: Add function to make grid and static color
    Static color is on hover only, not click. I would ideally like to make
    it a click and drag situation. Or a toggle situation (on click, hover to
    color. On click again, hover to color stops).
    
    Next step: add button to top of screen asking user for grid size, max
    100x100.

--- script.js
+++ script.js
@@ -3,4 +3,20 @@ const container = document.getElementById('container');
 function makeGrid(rows, columns) {
   container.style.setProperty('--grid-row', rows);
   container.style.setProperty('--grid-column', columns);
+  for (let i = 1; i <= (rows * columns); i++) {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    container.appendChild(cell);
+  }
+}
+
+makeGrid(5,5);
+
+let gridItems = document.querySelectorAll('.cell');
+for (let i = 0; i < gridItems.length; i++) {
+  gridItems[i].addEventListener('mouseover', makeBlue);
+}
+
+function makeBlue(e) {
+  e.target.style.backgroundColor = 'blue';
 }
