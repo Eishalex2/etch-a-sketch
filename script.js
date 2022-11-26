b57080dea569bd61c921b25b1a4b1bef977bb4a9
Eishalex2 <t.whitney.a@gmail.com>
2022-11-25 17:52:22 -0800

    wip: Make Grid works, but can't get color change
    Made a form input for the user to enter the dimensions of the grid and
    set up a function and a click event to get that to work.
    
    But for some reason, the color change no longer works. Backup is to set
    up a prompt window.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.getElementById('container');
+const dim = document.querySelector('input[type=number]');
+const btn = document.querySelector('input[type=submit]');
 
 function makeGrid(rows, columns) {
   container.style.setProperty('--grid-row', rows);
@@ -7,16 +9,29 @@ function makeGrid(rows, columns) {
     let cell = document.createElement('div');
     cell.classList.add('cell');
     container.appendChild(cell);
+    console.log(cell);
   }
 }
 
-makeGrid(5,5);
+let numCells = dim.value;
+
+//looks like <div class='cell'></div>
+btn.addEventListener('click', () => {
+  makeGrid(dim.value, dim.value);
+});
+
+//Also looks like <div class='cell'></div>
+// makeGrid(5,5);
 
 let gridItems = document.querySelectorAll('.cell');
 for (let i = 0; i < gridItems.length; i++) {
   gridItems[i].addEventListener('mouseover', makeBlue);
-}
+};
 
 function makeBlue(e) {
   e.target.style.backgroundColor = 'blue';
-}
+};
+
+// function makeBlue(e) {
+//   e.
+// }
